0e51a72d4002561c158fe4aa94c4bc6157b4bebc
olu26 <oluwork12@gmail.com>
2025-05-29 18:54:23 +0100

    trying to resolve deployment build issue

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import About from './pages/About';
 import Cart from './pages/Cart'; 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import SearchBar from './components/searchBar';
+import SearchBar from './components/SearchBar';
   import { ToastContainer,  } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
